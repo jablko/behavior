621dcd158fa350e2e9f837bec2b75131f6c95e11
Jack Bates <ms419@freezone.co.uk>
2010-02-22 16:37:59 -0800

    first commit

---
+++ csv.js
@@ -0,0 +1,17 @@
+(function ($)
+  {
+    $(function ()
+      {
+        $('.csv').replaceWith(function ()
+          {
+            return ['<table>',
+                '<tbody>',
+                  $(this).text()
+                    .replace(/^/gm, '<tr><td>')
+                    .replace(/$/gm, '</td></tr>')
+                    .replace(/,/g, '</td><td>'),
+                '</tbody>',
+              '</table>'].join('');
+          });
+      });
+  })(jQuery);
