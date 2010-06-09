201c5274ef4f9774c15c2fd2222c02fba456cde8
Jack Bates <ms419@freezone.co.uk>
2010-06-09 14:31:49 -0700

    Cosmetic change

--- section.js
+++ section.js
@@ -3,9 +3,9 @@ jQuery(function ($)
     $(':header').each(function ()
       {
         var selector = [];
-        for (var i = 1; i <= this.tagName.slice(-1); i++)
+        for (var level = 1; level <= this.tagName.slice(-1); level++)
         {
-          selector.push('h' + i);
+          selector.push('h' + level);
         }
 
         $(this).nextUntil(selector.join(', '))
