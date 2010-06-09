af7eb583497531072f9072f60ab470796734c2d9
Jack Bates <ms419@freezone.co.uk>
2010-06-09 14:28:27 -0700

    Use first argument of jQuery() callback for $ alias, http://api.jquery.com/jQuery/#jQuery3

--- csv.js
+++ csv.js
@@ -1,17 +1,14 @@
-(function ($)
+jQuery(function ($)
   {
-    $(function ()
+    $('.csv').replaceWith(function ()
       {
-        $('.csv').replaceWith(function ()
-          {
-            return ['<table>',
-                '<tbody>',
-                  $(this).text()
-                    .replace(/^/gm, '<tr><td>')
-                    .replace(/$/gm, '</td></tr>')
-                    .replace(/,/g, '</td><td>'),
-                '</tbody>',
-              '</table>'].join('');
-          });
+        return ['<table>',
+            '<tbody>',
+              $(this).text()
+                .replace(/^/gm, '<tr><td>')
+                .replace(/$/gm, '</td></tr>')
+                .replace(/,/g, '</td><td>'),
+            '</tbody>',
+          '</table>'].join('');
       });
-  })(jQuery);
+  });
